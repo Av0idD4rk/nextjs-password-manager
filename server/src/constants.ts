e6097af32d8c004c88afeaecdf94a393b6064abc
Av0idD4rk <mister57joy@gmail.com>
2024-11-26 08:49:13 +0300

    fix packages again

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,7 +1,7 @@
 export const DB_CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING ||
-  "mongodb://192.168.1.125:27023/passwordManager";
+  "mongodb://mongo:27017/passwordManager";
 
-export const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
+export const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://37.1.204.135";
 
-export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || "localhost";
+export const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || "37.1.204.135";
